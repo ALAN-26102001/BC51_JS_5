7bd87db890c8bbea2ca2aa8f4b91a6b4559ac9a3
ALAN <ntthanhnga2610@gmail.com>
2023-06-07 23:32:44 +0700

    upload source

---
+++ Bài 4/main.js
@@ -0,0 +1,10 @@
+/*Nhà Dân*/
+const ND_HOADON = 4.5;
+const ND_DICHVU = 20.5;
+const ND_THUEKENH = 7.5;
+
+/*Doanh Nghiệp*/
+const DN_HOADON = 15;
+const DN_DICHVU = 75;
+const DN_DICHVU_PLUS = 5;
+const DN_THUEKENH = 50;
